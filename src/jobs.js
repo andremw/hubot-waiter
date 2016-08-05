4eb63e97ffd352bda32287c30193edad8fe1ad6f
Andre Mazoni <mazoni.andre@gmail.com>
2016-08-05 16:59:07 -0300

    Adds job status command

---
+++ src/jobs.js
@@ -0,0 +1,72 @@
+'use strict';
+
+module.exports = {
+  listJobs(params) {
+    const robot = params.robot;
+    const jobFilter = params.response.match[1].trim();
+
+    robot.http(`${params.JENKINS_URL}/api/json`).get()((err, res, body) => {
+      if (err) {
+        robot.logger.error(err);
+        params.response.send('Sorry, I could not get the list of jobs.');
+        return;
+      }
+
+      let jobs = JSON.parse(body).jobs;
+
+      if (jobFilter) {
+        const regex = new RegExp(jobFilter, 'i');
+        jobs = jobs.filter(job => regex.test(job.name));
+      }
+
+      const formattedResponse = jobs.reduce((msg, job) => {
+        return `${msg}\n> ${job.name}`;
+      }, 'Here is the list of jobs:\n');
+
+      params.response.send(formattedResponse);
+    });
+  },
+
+  jobStatus(params) {
+    const robot = params.robot;
+    const jobName = params.jobName;
+    const jobUrl = `${params.JENKINS_URL}/job/${jobName}`;
+    const fullUrl = `${jobUrl}/api/json?tree=healthReport[description,iconUrl],lastBuild[number],lastFailedBuild[number]`;
+
+    robot.http(fullUrl).get()((err, res, body) => {
+      if (err) {
+        robot.logger.error(err);
+        params.response.send(`Sorry, I could not get the status of the job ${jobName}`);
+        return;
+      }
+
+      const jobJson = JSON.parse(body);
+      const healthReport = jobJson.healthReport[0];
+
+      const healthResponse = {
+        text: `Here's the health status for ${jobName}:`,
+        attachments: [
+          {
+            fallback: healthReport.description,
+            color: getMessageColor(jobJson),
+            title: jobName,
+            title_link: jobUrl,
+            text: healthReport.description,
+            footer: 'Jenkins API',
+            footer_icon: `${params.JENKINS_URL}/static/aeb9cf3a/images/32x32/${healthReport.iconUrl}`,
+            ts: new Date().getTime()
+          }
+        ]
+      };
+
+      params.response.send(healthResponse);
+    });
+  }
+};
+
+function getMessageColor(jobJson) {
+  if (jobJson.lastBuild.number === jobJson.lastFailedBuild.number) {
+    return 'danger';
+  }
+  return 'good';
+}
