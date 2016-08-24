1eb32c7480ab1b9d8efb518f37011d6ee5e33e50
Andre Mazoni <mazoni.andre@gmail.com>
2016-08-24 17:19:58 -0300

    Changes listJobs to pass credentials. Improves logging

--- src/jobs.js
+++ src/jobs.js
@@ -1,15 +1,28 @@
+/* eslint camelcase: 0 */
 'use strict';
 
-module.exports = {
-  listJobs(params) {
-    const robot = params.robot;
-    const jobFilter = params.response.match[1].trim();
+module.exports.listJobs = listJobs;
+module.exports.jobStatus = jobStatus;
 
-    robot.http(`${params.JENKINS_URL}/api/json`).get()((err, res, body) => {
+function listJobs(params) {
+  const robot = params.robot;
+  const jobFilter = params.response.match[1].trim();
+  const user = params.response.message.user;
+  const credentials = (params.robot.brain.get('jenkinsWaiter') || {credentials: {}}).credentials[user] || {};
+
+  robot
+    .http(`${params.JENKINS_URL}/api/json`)
+    .auth(credentials.user, credentials.apiKey)
+    .get()((err, res, body) => {
+      params.robot.logger.info(`Tried to retrieve jobs filtering by '${jobFilter || 'no filter'}'.\nErr: ${err}\nStatusCode: ${res.statusCode}\nStatus Message: ${res.statusMessage}`);
       if (err) {
-        robot.logger.error(err);
-        params.response.send('Sorry, I could not get the list of jobs.');
-        return;
+        return params.response.send('Sorry, I could not get the list of jobs.');
+      }
+      if (res.statusCode === 403) {
+        return params.response.send(`You're not authorized to retrieve the jobs. Are you authenticated?`);
+      }
+      if (res.statusCode !== 201 && res.statusCode !== 200) {
+        return params.response.send(`Got a ${res.statusCode} error while retrieving the jobs: ${res.statusMessage}`);
       }
 
       let jobs = JSON.parse(body).jobs;
@@ -25,44 +38,43 @@ module.exports = {
 
       params.response.send(formattedResponse);
     });
-  },
+}
 
-  jobStatus(params) {
-    const robot = params.robot;
-    const jobName = params.jobName;
-    const jobUrl = `${params.JENKINS_URL}/job/${jobName}`;
-    const fullUrl = `${jobUrl}/api/json?tree=healthReport[description,iconUrl],lastBuild[number],lastFailedBuild[number]`;
+function jobStatus(params) {
+  const robot = params.robot;
+  const jobName = params.jobName;
+  const jobUrl = `${params.JENKINS_URL}/job/${jobName}`;
+  const fullUrl = `${jobUrl}/api/json?tree=healthReport[description,iconUrl],lastBuild[number],lastFailedBuild[number]`;
 
-    robot.http(fullUrl).get()((err, res, body) => {
-      if (err) {
-        robot.logger.error(err);
-        params.response.send(`Sorry, I could not get the status of the job ${jobName}`);
-        return;
-      }
+  robot.http(fullUrl).get()((err, res, body) => {
+    if (err) {
+      robot.logger.error(err);
+      params.response.send(`Sorry, I could not get the status of the job ${jobName}`);
+      return;
+    }
 
-      const jobJson = JSON.parse(body);
-      const healthReport = jobJson.healthReport[0] || {};
+    const jobJson = JSON.parse(body);
+    const healthReport = jobJson.healthReport[0] || {};
 
-      const healthResponse = {
-        attachments: [
-          {
-            fallback: healthReport.description,
-            color: getMessageColor(jobJson),
-            title: jobName,
-            title_link: jobUrl,
-            text: healthReport.description,
-            footer: 'Jenkins API',
-            footer_icon: `${params.JENKINS_URL}/static/aeb9cf3a/images/32x32/${healthReport.iconUrl}`,
-            ts: getUnixTime()
-          }
-        ],
-        as_user: true
-      };
+    const healthResponse = {
+      attachments: [
+        {
+          fallback: healthReport.description,
+          color: getMessageColor(jobJson),
+          title: jobName,
+          title_link: jobUrl,
+          text: healthReport.description,
+          footer: 'Jenkins API',
+          footer_icon: `${params.JENKINS_URL}/static/aeb9cf3a/images/32x32/${healthReport.iconUrl}`,
+          ts: getUnixTime()
+        }
+      ],
+      as_user: true
+    };
 
-      params.response.send(healthResponse);
-    });
-  }
-};
+    params.response.send(healthResponse);
+  });
+}
 
 function getMessageColor(jobJson) {
   const lastBuild = jobJson.lastBuild || {number: 0};
